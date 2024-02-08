b8b582d33e7d37762ae24135e2b12fcda168b654
SF <fey@ambihome.com>
2024-02-08 08:00:57 +0100

    added ix

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,13 @@ import '@fontsource-variable/jetbrains-mono'
 import './lib/phoenix'
 import './lib/phoenix_live_view'
 import './lib/tailwind'
+
+import '@siemens/ix/dist/siemens-ix/siemens-ix.css';
+import { applyPolyfills, defineCustomElements } from '@siemens/ix/loader';
+import { defineCustomElements as ixIconsDefineCustomElements } from '@siemens/ix-icons/loader';
+
+(async () => {
+  await applyPolyfills();
+  await ixIconsDefineCustomElements();
+  await defineCustomElements();
+})();
--- lib/hello_web/live/example_live.ex
+++ lib/hello_web/live/example_live.ex
@@ -3,12 +3,17 @@ defmodule HelloWeb.ExampleLive do
 
   def render(assigns) do
     ~H"""
-    <div class="font-sans">
+    <div class="font-sans bg-cyan-400">
       Hello, font-sans.
     </div>
 
-    <div class="font-mono">
+    <div class="font-mono bg-lime-400">
       Hello, font-mono.
+    </div>
+
+    <div>
+      <ix-button class="m-1" variant="primary">Button</ix-button>
+      <ix-button class="m-1" variant="primary" disabled>Button</ix-button>
     </div>
     """
   end
